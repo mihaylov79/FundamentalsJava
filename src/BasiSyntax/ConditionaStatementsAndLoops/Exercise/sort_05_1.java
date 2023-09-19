7bb45ff901ee4dcbb82150f2d4e4c9551a8928e3
Mihaylov <misho.mihaylov@gmail.com>
2023-09-19 23:19:03 +0300

    Update

---
+++ src/BasiSyntax/ConditionaStatementsAndLoops/Exercise/sort_05_1.java
@@ -0,0 +1,49 @@
+package BasiSyntax.ConditionaStatementsAndLoops.Exercise;
+
+import java.util.Scanner;
+
+public class sort_05_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(scan.nextLine());
+        int num2 = Integer.parseInt(scan.nextLine());
+        int num3 = Integer.parseInt(scan.nextLine());
+
+        if ((num1 > num2 && num1 > num3))
+        {
+            if(num2 > num3)
+            {
+                System.out.printf("%d%n%d%n%d",num1,num2,num3);
+            }
+            else
+                System.out.printf("%d%n%d%n%d",num1,num3,num2);
+        }
+        else if ((num2 > num1 && num2 > num3))
+        {
+            if(num1 > num3)
+            {
+                System.out.printf("%d%n%d%n%d",num2,num1,num3);
+            }
+            else
+            {
+                System.out.printf("%d%n%d%n%d",num2,num3,num1);
+            }
+        }
+        else if ((num3 > num1 && num3 > num2))
+        {
+            if(num1 > num2)
+            {
+                System.out.printf("%d%n%d%n%d",num3,num1,num2);
+            }
+            else
+                System.out.printf("%d%n%d%n%d",num3,num2,num1);
+        }
+        else
+        {
+            System.out.println("ERROR!");
+        }
+
+    }
+
+}
