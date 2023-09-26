197456bb07d446baf7d393cc598cc29752e33847
Mihail Mihaylov <misho.mihaylov@gmail.com>
2023-09-26 15:41:10 +0300

    Update - pr.6 need to be finished

--- src/DataTypesAndVariables/Exersices/BalancedBrackets_06_ME.java
+++ src/DataTypesAndVariables/Exersices/BalancedBrackets_06_ME.java
@@ -9,9 +9,9 @@ public class BalancedBrackets_06_ME {
         int lines = Integer.parseInt(kbInput.nextLine());
         int left1 = 0;
         int right1 = 0;
+        boolean balanced = true;
 
         for (int i = 0; i < lines; i++) {
-
             char contains = kbInput.nextLine().charAt(0);
 
             if (contains == '('){
@@ -20,11 +20,20 @@ public class BalancedBrackets_06_ME {
             if (contains == ')'){
                 right1++;
             }
+            if (left1 > right1 && left1 - right1 > 1 ||right1 > left1 && right1 - left1 > 1){
+                balanced = false;
+            }
         }
-        if (right1 == left1){
+        if (right1 == left1 && balanced){
             System.out.println("BALANCED");
-        }else {
+        }
+        if (right1 != left1 ){
             System.out.println("UNBALANCED");
         }
+        //if (balanced){
+            //System.out.println("BALANCED");
+        //}else {
+            //System.out.println("UNBALANCED");
+        //}
     }
 }
