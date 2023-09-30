0ac03bcec29da7f972d5a83d9ec0f61aef28e9c2
Mihaylov <misho.mihaylov@gmail.com>
2023-09-30 06:20:08 +0300

    Update

---
+++ src/Arrays/Exercises/Train_01.java
@@ -0,0 +1,27 @@
+package Arrays.Exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Train_01 {
+    public static void main(String[] args) {
+        Scanner kbInput = new Scanner(System.in);
+
+        int wagons = Integer.parseInt(kbInput.nextLine());
+
+        int[] people = new int[wagons];
+        int peopleSum = 0;
+
+        for (int i = 0; i < wagons; i++) {
+
+            people[i] = Integer.parseInt(kbInput.nextLine());
+            peopleSum += people[i];
+        }
+        System.out.print(Arrays.toString(people)
+                .replace("[","")
+                .replace("]",""));
+        System.out.println();
+        System.out.println(peopleSum);
+
+    }
+}
