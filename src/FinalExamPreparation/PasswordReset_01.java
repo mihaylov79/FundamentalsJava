aad5a884d9652843c4e8bb72e17c128992c51efb
Mihaylov <misho.mihaylov@gmail.com>
2023-11-23 02:30:13 +0200

    Update

---
+++ src/FinalExamPreparation/PasswordReset_01.java
@@ -0,0 +1,20 @@
+package FinalExamPreparation;
+
+import java.util.Scanner;
+
+public class PasswordReset_01 {
+    public static void main(String[] args) {
+        Scanner kbInput = new Scanner(System.in);
+
+        String stringInput = kbInput.nextLine();
+
+        String commandInput = kbInput.nextLine();
+
+        while (!"Done".equalsIgnoreCase(commandInput)){
+
+
+        }
+
+
+    }
+}
